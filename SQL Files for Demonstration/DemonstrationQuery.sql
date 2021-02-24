677bd454c6f959a86b1bc95d6c99f4b9ce9080e2
westondavidson <weston@westondavidson.com>
2021-02-24 17:56:23 -0500

    updated SQL query with Aggregate function code demo

--- SQL Files for Demonstration/DemonstrationQuery.sql
+++ SQL Files for Demonstration/DemonstrationQuery.sql
@@ -19,6 +19,31 @@ select * from people;
 
 -- Weston - Aggregate
 
+-- The cast functions convert all values within the specified column to the specified value type
+-- the avg function averages all values in said column to a specific value.
+-- then, the final two casts return the value to it's original data formatting
+SELECT CAST(
+			CAST(
+					AVG(
+						CAST(
+							--inner layer - cast the Date type to a datetime type
+							--datetime tracks individual day values
+							CAST(
+								birthDate AS DATETIME
+								) 
+							--middle layer - cast to int, which converts to a total number of days
+							AS INT)
+						--average the number of days each column has
+						) 
+					-- return the new int average back to datetime format
+					AS DATETIME) 
+			-- and finally, return it to it's original formatting
+			AS DATE) 
+-- name the column something
+AS averageBirthday
+FROM people;
+
+
 -- Alan - Tabular
 
 -- Frank - Custom
