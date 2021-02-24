bdd54c03ab38443bd206fce61545c7b8c0aa47a3
westondavidson <weston@westondavidson.com>
2021-02-24 17:59:45 -0500

    updated SQL query with aggregate function code demo

--- SQL Files for Demonstration/DemonstrationQuery.sql
+++ SQL Files for Demonstration/DemonstrationQuery.sql
@@ -9,7 +9,7 @@ CREATE TABLE people (
 
 --Adding seed data
 insert into people (name, birthDate) values 
-('Kevin', '1996-11-12'), ('Alan', '1994-04-13'),('Weston', '1997-07-08'),('Kevin', '1996-02-01'),('Steve', '1995-09-26');
+('Kevin', '1996-11-12'), ('Alan', '1994-04-13'),('Weston', '1997-07-08'),('Frank', '1996-02-01'),('Steve', '1995-09-26');
 
 select * from people;
 
